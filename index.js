e8a3c9f80e6b60602d559208efe60c84ba066257
DominoSl <artemenko.mik@gmail.com>
2020-09-11 14:42:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
+
+// Calculation:
+function result() {
+    let bill = document.getElementById('bill').value;
+    let tip = parseInt(document.getElementById('tip').value, 10)* bill /100;
+    let number = document.getElementById('number').value;
+    let tipper = tip / number;
+    let total= bill -(-tip);
+    let totalper = total / number;
+    document.getElementById('tipf').innerHTML = tipper;
+    document.getElementById('total').innerHTML = totalper;
+} 
+
+//Buttons
+
+document.getElementById('tm').addEventListener('click', function() {
+    document.getElementById('tip').value =  parseInt(document.getElementById('tip').value, 10) - 1 + '%';
+}, false)
+
+document.getElementById('tp').addEventListener('click', function() {
+    document.getElementById('tip').value =  parseInt(document.getElementById('tip').value, 10) + 1 + '%';
+}, false)
+
+document.getElementById('nm').addEventListener('click', function() {
+    document.getElementById('number').value =  parseInt(document.getElementById('number').value, 10) - 1 ;
+}, false)
+
+document.getElementById('np').addEventListener('click', function() {
+    document.getElementById('number').value =  parseInt(document.getElementById('number').value, 10) + 1;
+}, false)
+
+
+//Eventlisteners
+
+document.getElementById('bill').addEventListener('change', result(), false)
+document.getElementById('tip').addEventListener('change', result(), false)
+document.getElementById('number').addEventListener('change', result(), false)
+
+
+
+
+
+
+//Trash
+
 // function tipvalue() {
 //     let tv = parseInt(document.getElementById('bill').value, 10) * parseInt(document.getElementById('tip').value, 10) /100;
 //     console.log(tv);
@@ -12,17 +57,6 @@
 //     document.getElementById('tip').innerHTML = 1056;
     // return eval(document.getElementById('bill').value / document.getElementById('number'));
 // }, false)
-
-let bill = parseInt(document.getElementById('bill'), 10);
-let tip = parseInt(document.getElementById('tip'), 10);
-let number = parseInt(document.getElementById('number'), 10);
-function qw() {
-    document.getElementById('tipf').innerHTML = bill;
-} 
-
-// document.getElementById('bill').addEventListener('change', result(), false)
-// document.getElementById('tip').addEventListener('change', result(), false)
-// document.getElementById('number').addEventListener('change', result(), false)
 
 
 // function result() {
