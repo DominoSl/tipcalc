67ccfca7f5235600cff05fe685e0a3c37d692593
DominoSl <artemenko.mik@gmail.com>
2020-09-11 10:40:38 +0300

    tr5ge

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function tipvalue() {
+    let tv = parseInt(document.getElementById('bill').value, 10) * parseInt(document.getElementById('tip').value, 10) /100;
+    console.log(tv);
+}
+
+
+function add(a, b) {
+    var c = a + b;
+    return c;
+}
--- js.js
+++
@@ -1,3 +0,0 @@
-document.calculate.addEventListener('click', function() {
-document.total.innerhtml = document.bill.innerhtml / document.number.innerhtml;
-}, false);
